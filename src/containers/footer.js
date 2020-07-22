2e07ff1f5dfcddba28685ec01ed015e89be05e6c
dejanlu <dejan.bl@gmail.com>
2020-07-22 23:12:30 +0200

    Add footer navigation

--- src/containers/footer.js
+++ src/containers/footer.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "gatsby";
 
 const Footer = () => {
-  return <footer data-testid="footer">Footer</footer>;
+  return (
+    <footer data-testid="footer" className="footer">
+      <nav className="footer__nav">
+        <Link to="/">Početna</Link>
+        <Link to="/about">O nama</Link>
+        <Link to="/offer">Ponuda objekata</Link>
+        <Link to="/blog">Naš blog</Link>
+      </nav>
+      <p className="footer__cop paragraph heading--light">
+        Copyright © Domus™ 1997 - 2020. Sva prava su zadržana.
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
