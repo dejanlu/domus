4281ea919d5d0f2bc41161c55023c147f434e66e
dejanlu <dejan.bl@gmail.com>
2020-07-23 17:01:54 +0200

    Add carousel component and config for it

--- src/components/message.js
+++ src/components/message.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const Message = ({ message, author }) => {
   return (
-    <article className="message">
+    <div className="message">
       <span className="message__quotes message__quotes--open">“</span>
       <p className="paragraph heading--light">{message}</p>
       <span className="message__quotes message__quotes--close">”</span>
       <p className="paragraph heading--light message__author"> - {author}</p>
-    </article>
+    </div>
   );
 };
 
--- src/containers/indexMessage.js
+++ src/containers/indexMessage.js
@@ -1,7 +1,11 @@
 import React from "react";
+import Slider from "react-slick";
 
 import content from "../content";
 import Message from "../components/message";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const renderMessages = (messagesArray) =>
   messagesArray.map((message) => (
@@ -11,9 +15,20 @@ const renderMessages = (messagesArray) =>
 const IndexMessage = () => {
   const data = content.getContent("poruke");
 
+  const sliderConfig = {
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
     <section data-testid="indexMessage" className="hero-messages">
-      {renderMessages(data)}
+      <Slider {...sliderConfig}>{renderMessages(data)}</Slider>
     </section>
   );
 };
