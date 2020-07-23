0e46df04fc19df4ad55ef73cda75d1bfce9e3f88
dejanlu <dejan.bl@gmail.com>
2020-07-23 02:35:59 +0200

    Add message component

--- src/containers/indexMessage.js
+++ src/containers/indexMessage.js
@@ -1,9 +1,19 @@
 import React from "react";
 
+import content from "../content";
+import Message from "../components/message";
+
+const renderMessages = (messagesArray) =>
+  messagesArray.map((message) => (
+    <Message key={message.ime} author={message.ime} message={message.text} />
+  ));
+
 const IndexMessage = () => {
+  const data = content.getContent("poruke");
+
   return (
-    <section data-testid="indexMessage" className="message">
-      Message
+    <section data-testid="indexMessage" className="hero-messages">
+      {renderMessages(data)}
     </section>
   );
 };
--- src/content/index.js
+++ src/content/index.js
@@ -1,9 +1,11 @@
 import features from "./features.json";
 import objekti from "./objekti.json";
+import poruke from "./poruke.json";
 
 const allContent = {
   features,
   objekti,
+  poruke,
 };
 
 const content = { getContent: (name) => allContent[name] };
